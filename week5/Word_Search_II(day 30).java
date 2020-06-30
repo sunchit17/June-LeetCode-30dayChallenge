fa941c992ea2ede8cf0c7e99d10d3bd1ad0e5990
Sunchit Lakhanpal <43404214+sunchit17@users.noreply.github.com>
2020-06-30 21:34:46 +0530

    Create Word_Search_II(day 30).java

---
+++ week5/Word_Search_II(day 30).java
@@ -0,0 +1,85 @@
+/*
+Given a 2D board and a list of words from the dictionary, find all words in the board.
+
+Each word must be constructed from letters of sequentially adjacent cell, 
+where "adjacent" cells are those horizontally or vertically neighboring. 
+The same letter cell may not be used more than once in a word.
+
+
+Example:
+Input: 
+board = [
+  ['o','a','a','n'],
+  ['e','t','a','e'],
+  ['i','h','k','r'],
+  ['i','f','l','v']
+]
+words = ["oath","pea","eat","rain"]
+
+Output: ["eat","oath"]
+*/
+
+public class Solution {
+
+    public class TrieNode{
+        public boolean isWord = false;
+        public TrieNode[] child = new TrieNode[26];
+        public TrieNode(){
+            
+        }
+    }
+    
+    TrieNode root = new TrieNode();
+    boolean[][] flag;
+    public List<String> findWords(char[][] board, String[] words) {
+        
+        Set<String> result = new HashSet<>();
+        flag = new boolean[board.length][board[0].length];
+        
+        addToTrie(words);
+        
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(root.child[board[i][j] - 'a'] != null){
+                    search(board, i, j, root, "", result);
+                }
+            }
+        }
+        
+        return new LinkedList<>(result);
+    }
+    
+    private void addToTrie(String[] words){
+        for(String word: words){
+            TrieNode node = root;
+            for(int i = 0; i < word.length(); i++){
+                char ch = word.charAt(i);
+                if(node.child[ch - 'a'] == null){
+                    node.child[ch - 'a'] = new TrieNode();
+                }
+                node = node.child[ch - 'a'];
+            }
+            node.isWord = true;
+        }
+    }
+    
+    private void search(char[][] board, int i, int j, TrieNode node, String word, Set<String> result){
+    
+        if(i >= board.length || i < 0 || j >= board[i].length || j < 0 || flag[i][j]|| node.child[board[i][j] - 'a'] == null){
+            return;
+        }
+        
+        flag[i][j] = true;
+        node = node.child[board[i][j] - 'a'];
+        if(node.isWord){
+            result.add(word + board[i][j]);
+        }
+        
+        search(board, i-1, j, node, word + board[i][j], result);
+        search(board, i+1, j, node, word + board[i][j], result);
+        search(board, i, j-1, node, word + board[i][j], result);
+        search(board, i, j+1, node, word + board[i][j], result);
+        
+        flag[i][j] = false;
+    }
+}
